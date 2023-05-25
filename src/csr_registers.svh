3944046c61293402aead385b2920990eff0160bc
sumiravishan <99382520+sumiravishan@users.noreply.github.com>
2023-05-25 08:19:19 +0530

    Update csr_registers.svh

--- src/csr_registers.svh
+++ src/csr_registers.svh
@@ -1,3 +1,29 @@
+/////////////////////////////////////////////////////////////////////
+////                                                             ////
+////  Author: Sumira Fernando                                    ////
+////          k.w.s.v.fernando@gmail.com                         ////
+////                                                             ////
+////                                                             ////
+/////////////////////////////////////////////////////////////////////
+////                                                             ////
+//// Copyright (C) 2023                                          ////
+////                                                             ////
+//// This source file may be used and distributed without        ////
+//// restriction provided that this copyright statement is not   ////
+//// removed from the file and that any derivative work contains ////
+//// the original copyright notice and the associated disclaimer.////
+////                                                             ////
+//// This source file is free software; you can redistribute it  ////
+//// and/or modify it under the terms of the GNU Lesser General  ////
+//// Public License as published by the Free Software Foundation.////
+////                                                             ////
+//// This source is distributed in the hope that it will be      ////
+//// useful, but WITHOUT ANY WARRANTY; without even the implied  ////
+//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR     ////
+//// PURPOSE.  See the GNU Lesser General Public License for more////
+//// details. http://www.gnu.org/licenses/lgpl.html              ////
+////                                                             ////
+/////////////////////////////////////////////////////////////////////
 class csr_registers extends uvm_object;
 	bit [31:0] csr[4095:0];
 	
